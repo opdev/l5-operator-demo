2b6188389941db472b4cdb97537c6746e011088b
Yuri Sa <yurimsa@gmail.com>
2022-05-04 14:16:09 +0200

    Using new l5-operator images
    Signed-off-by: Yuri Sa <yurimsa@gmail.com>

--- hack/install-pgo.sh
+++ hack/install-pgo.sh
@@ -3,3 +3,5 @@
 sudo git clone https://github.com/CrunchyData/postgres-operator-examples.git
 
 kubectl apply -k postgres-operator-examples/kustomize/install/namespace
+
+kubectl apply --server-side -k postgres-operator-examples/kustomize/install/default

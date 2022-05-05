ea7b7bd45508cbd44b8677fc64ff89c033dd1589
Yuri Sa <yurimsa@gmail.com>
2022-05-05 13:59:11 +0200

    Removing duplicate pgo install and replicas field
    Signed-off-by: Yuri Sa <yurimsa@gmail.com>

--- hack/install-pgo.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo git clone https://github.com/CrunchyData/postgres-operator-examples.git
-
-kubectl apply -k postgres-operator-examples/kustomize/install/namespace
-
-kubectl apply --server-side -k postgres-operator-examples/kustomize/install/default
